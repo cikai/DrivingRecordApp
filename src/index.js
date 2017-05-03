41dde1242790e1733598332b88b81bfb4a57c356
cikai <cikai0923@gmail.com>
2017-05-03 16:52:47 +0800

    android 'back' press

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import React, {Component} from "react";
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  ToastAndroid,
+  BackAndroid,
+  Platform
 } from "react-native";
 import {
   Navigation,
@@ -52,6 +55,27 @@ export default class App extends Component {
   constructor(props) {
     super(props);
   }
+
+  componentWillMount() {
+    if (Platform.OS === 'android') {
+      BackAndroid.addEventListener('hardwareBackPress', this.onBackAndroid);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackAndroid.removeEventListener('hardwareBackPress', this.onBackAndroid);
+    }
+  }
+
+  onBackAndroid = () => {
+    if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+      return false;
+    }
+    this.lastBackPressed = Date.now();
+    ToastAndroid.show('再按一次退出应用');
+    return true;
+  };
 
   render() {
     return (
